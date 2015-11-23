d25c9d75428a3fea772a35f2dac05652c1ca70f8
Mathias Nigsch <mathias.nigsch@gmail.com>
2015-11-23 16:23:41 +0100

    Added closing bean

---
+++ modules/Closing/src/ClosingBeanInfo.java
@@ -0,0 +1,27 @@
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+/**
+ * Created by Mathias on 23/11/2015.
+ */
+public class ClosingBeanInfo extends SimpleBeanInfo {
+    private final static Class beanClass = ClosingBeanInfo.class;
+
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        try {
+            PropertyDescriptor radius = new PropertyDescriptor("radius", beanClass);
+            radius.setBound(true);
+
+            PropertyDescriptor[] propertyDescriptors = {radius};
+            return propertyDescriptors;
+
+        } catch (IntrospectionException e) {
+            throw new Error(e.toString());
+        }
+    }
+
+    public int getDefaultPropertyIndex() {
+        return 0;
+    }
+}
